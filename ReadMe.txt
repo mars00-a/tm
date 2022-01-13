b5fddeb7f43d1eb679283559948b440ae253e4c3
“wang-xin” <564081599@qq.com>
2022-01-13 17:28:20 +0800

    优化结构

---
+++ ReadMe.txt
@@ -0,0 +1,12 @@
+请按照代码库中的文件分类存放代码
+代码的命名一定要规范易读懂
+每人开发单独新建一个分支，需要提交时再合并到主分支进行提交
+
+html中的fonts是字体图标，使用https://icomoon.io/网站下载
+具体的使用方式请参考OneNote笔记中字体图标分区
+
+此仓库网址为https://gitee.com/wangxin564081/tm.git
+
+如果push文件的时候报错了
+可以先使用git pull --rebase origin master将仓库代码同步到本地 然后再push
+建议每次敲代码之前先同步一下这样可以防止代码被改过后再想提交十分麻烦
---
+++ frontEnd/javascript/register.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+  $("#footer1").load("./footer1.html");
+  $("#footer2").load("./footer2.html");
+  $("#nav").load("./nav.html");
+});
+
+function check() {
+    var name = document.forms["info"]["name"].value;
+    var password = document.forms["info"]["password"].value;
+    var repeatpassword = document.forms["info"]["repeatpassword"].value;
+    var x = document.getElementById("alert")
+    if (name == null || name == "") {
+        x.style.border = "1px solid #ebccd1";
+        x.style.backgroundColor = "#f2dede"
+        document.getElementById("spanalert").innerHTML = "请输入用户名";
+        return false;
+    }
+    else if (password == null || password == "") {
+        x.style.border = "1px solid #ebccd1";
+        x.style.backgroundColor = "#f2dede"
+        document.getElementById("spanalert").innerHTML = "请输入密码";
+        return false;
+    }
+    else if (repeatpassword == null || repeatpassword == "") {
+        x.style.border = "1px solid #ebccd1";
+        x.style.backgroundColor = "#f2dede"
+        document.getElementById("spanalert").innerHTML = "请再次输入密码";
+        return false;
+    }
+    else if (password.length < 8) {
+        x.style.border = "1px solid #ebccd1";
+        x.style.backgroundColor = "#f2dede"
+        document.getElementById("spanalert").innerHTML = "密码长度小于八位";
+        return false;
+    }
+    else if (password != repeatpassword) {
+        x.style.border = "1px solid #ebccd1";
+        x.style.backgroundColor = "#f2dede"
+        document.getElementById("spanalert").innerHTML = "两次密码不一致";
+        return false;
+    }
+}
