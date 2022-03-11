676158cb522f6595ea785349fb80d5b1924c60e6
“wang-xin” <564081599@qq.com>
2022-03-11 11:28:52 +0800

    提交

---
+++ frontEnd/javascript/forepayed.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $("#footer1").load("./footer1.html");
+  $("#footer2").load("./footer2.html");
+  $("#nav").load("./nav.html");
+});
--- frontEnd/javascript/register.js
+++ frontEnd/javascript/register.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $("#footer1").load("./footer1.html");
   $("#footer2").load("./footer2.html");
   $("#nav").load("./nav.html");
